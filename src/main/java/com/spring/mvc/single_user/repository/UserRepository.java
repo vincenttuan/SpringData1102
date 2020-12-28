474d1306ba0c50b890c5eb9e4c06a0ba54baeb50
teacher <teacher@192.168.1.180>
2020-12-28 19:30:56 +0800

    SpringData

--- src/main/java/com/spring/mvc/single_user/repository/UserRepository.java
+++ src/main/java/com/spring/mvc/single_user/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /*
+只需定義 interface 的規格, 實作內容是由 SpringData 幫你做
 <User, Long> -> User 指的是Entity的類別名, Long 指的是 @Id 的資料型別
 */
 @Repository
