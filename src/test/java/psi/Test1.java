a3988a52d7ee76cc8ebcf0aec4b8a199fed82fe5
teacher <teacher@192.168.1.180>
2021-01-04 20:11:01 +0800

    SpringData

--- src/test/java/psi/Test1.java
+++ src/test/java/psi/Test1.java
@@ -2,6 +2,7 @@ package psi;
 
 import com.spring.mvc.psi.entities.Product;
 import com.spring.mvc.psi.entities.Purchase;
+import com.spring.mvc.psi.entities.Sales;
 import com.spring.mvc.psi.repository.ProductRepository;
 import com.spring.mvc.psi.repository.PurchaseRepository;
 import com.spring.mvc.psi.repository.SalesRepository;
@@ -29,12 +30,19 @@ public class Test1 {
         Product p1 = pr.findOne(1);
         Product p2 = pr.findOne(2);
         Product p3 = pr.findOne(3);
-        Purchase u1 = new Purchase(10, 20, p1); // price, quantity, product
-        Purchase u2 = new Purchase(15, 25, p2);
-        Purchase u3 = new Purchase(20, 30, p3);
-        Purchase u4 = new Purchase(15, 30, p1);
-        Purchase u5 = new Purchase(20, 10, p2);
-        ur.save(Arrays.asList(u1, u2, u3, u4, u5));
+//        Purchase u1 = new Purchase(10, 20, p1); // price, quantity, product
+//        Purchase u2 = new Purchase(15, 25, p2);
+//        Purchase u3 = new Purchase(20, 30, p3);
+//        Purchase u4 = new Purchase(15, 30, p1);
+//        Purchase u5 = new Purchase(20, 10, p2);
+//        ur.save(Arrays.asList(u1, u2, u3, u4, u5));
+        // 賣出商品
+        Sales s1 = new Sales(50, 20, p1);
+        Sales s2 = new Sales(70, 10, p2);
+        Sales s3 = new Sales(100, 20, p3);
+        Sales s4 = new Sales(120, 10, p3);
+        Sales s5 = new Sales(50, 10, p1);
+        sr.save(Arrays.asList(s1, s2, s3, s4, s5));
         System.out.println("OK");
         
     }
