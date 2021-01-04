fd6f4e62bce455c3079289b35c0e877b965e2201
teacher <teacher@192.168.1.180>
2021-01-04 19:43:45 +0800

    SpringData

--- src/test/java/psi/Test1.java
+++ src/test/java/psi/Test1.java
@@ -1,5 +1,10 @@
 package psi;
 
+import com.spring.mvc.psi.entities.Product;
+import com.spring.mvc.psi.repository.ProductRepository;
+import com.spring.mvc.psi.repository.PurchaseRepository;
+import com.spring.mvc.psi.repository.SalesRepository;
+import java.util.Arrays;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,7 +12,18 @@ public class Test1 {
     @Test
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("springdata-jpa-config.xml");
-        System.out.println(ctx);
+        ProductRepository pr = ctx.getBean(ProductRepository.class);
+        PurchaseRepository ur = ctx.getBean(PurchaseRepository.class);
+        SalesRepository sr = ctx.getBean(SalesRepository.class);
+        Product p1 = new Product();
+        p1.setName("Apple");
+        Product p2 = new Product();
+        p2.setName("Orange");
+        Product p3 = new Product();
+        p3.setName("Mango");
+        pr.save(Arrays.asList(p1, p2, p3));
+        
+        System.out.println("OK");
         
     }
 }
