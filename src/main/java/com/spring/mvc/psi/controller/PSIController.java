14b5e017a052c8abee94a215355ae10bb4500055
teacher <teacher@192.168.1.180>
2021-01-08 20:05:17 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/controller/PSIController.java
+++ src/main/java/com/spring/mvc/psi/controller/PSIController.java
@@ -6,10 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -45,6 +47,20 @@ public class PSIController {
     @PostMapping(value = {"/product"})
     public String createProduct(@ModelAttribute("product") Product product) {
         pr.saveAndFlush(product);
+        return "redirect: ../psi/product";
+    }
+    
+    // 修改商品
+    @PutMapping(value = {"/product"})
+    public String updateProduct(@ModelAttribute("product") Product product) {
+        pr.saveAndFlush(product);
+        return "redirect: ../psi/product";
+    }
+    
+    // 刪除商品
+    @DeleteMapping(value = {"/product"})
+    public String deleteProduct(@ModelAttribute("product") Product product) {
+        pr.delete(product.getId());
         return "redirect: ../psi/product";
     }
     
