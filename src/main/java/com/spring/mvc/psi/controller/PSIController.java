986e069dc115d1575dbfec5837c46dd829d50cdf
teacher <teacher@192.168.1.180>
2021-01-08 19:21:41 +0800

    SpringData

--- src/main/java/com/spring/mvc/psi/controller/PSIController.java
+++ src/main/java/com/spring/mvc/psi/controller/PSIController.java
@@ -1,6 +1,8 @@
 package com.spring.mvc.psi.controller;
 
+import com.spring.mvc.psi.entities.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +13,9 @@ public class PSIController {
     
     // 讀取商品資料
     @GetMapping(value = {"/product"})
-    public String readProduct() {
+    public String readProduct(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
         return "psi/product";
     }
     
